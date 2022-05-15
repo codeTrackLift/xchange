df07d6e33998fd173fa460a4932cff287aa0fa3f
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-05-15 12:47:08 -0400

    flist wordsm

--- src/components/partials/FooterList.js
+++ src/components/partials/FooterList.js
@@ -1,12 +1,12 @@
 const footerList = [
     ['MIT | xPRO', 'https://xpro.mit.edu/'],
-    ['Full Stack Bootcamp', 'https://xpro.mit.edu/programs/program-v1:xPRO+PCCx+R1/'],
+    ['Coding Bootcamp', 'https://xpro.mit.edu/programs/program-v1:xPRO+PCCx+R1/'],
     ['MERN Development', 'https://executive-ed.xpro.mit.edu/professional-certificate-coding'],
     ['GitHub Readme', 'https://github.com/codeTrackLift/xchange#readme'],
     ['Developer Portfolio', 'https://www.codebypete.com/'],
     ['About Us', 'https://www.codebypete.com/pages/about.html'],
     ['Blog Articles', 'https://www.codebypete.com/pages/blog.html'],
-    ['Projects', 'https://www.codebypete.com/pages/projects.html'],
+    ['Our Projects', 'https://www.codebypete.com/pages/projects.html'],
     ['Careers', 'https://www.codebypete.com/pages/resume.html'],
     ['Contact Us', 'https://www.codebypete.com/pages/contact.html'],
 ]
